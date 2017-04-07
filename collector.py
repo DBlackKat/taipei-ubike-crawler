4c7efc2556dc0f6e27873431335eb8b50449c84f
DBlackKat <zhirui09400@gmail.com>
2017-04-07 07:29:13 +0000

    new crawler format XD

--- collector.py
+++ collector.py
@@ -1,19 +1,20 @@
 import urllib, json
 import requests
 import time
+
 if __name__ == "__main__":
-	url = "http://data.ntpc.gov.tw/api/v1/rest/datastore/382000000A-000352-001"
-	try:
-		lastDay = u'2017'
-		while(True):
-			data = json.loads(requests.get("http://data.taipei/youbike").content)
-			if data['retVal']['0001']['mday'] != lastDay:
-				lastDay = data['retVal']['0001']['mday']
-				with open(lastDay+'.json', 'w') as json_file:
-					json.dump(data,json_file)
-				print 'data renew!' + lastDay
-			time.sleep(30)
-	except:
-		pass
+	lastDay = u'2017'
+	while(True):
+		try:
+			while(True):
+				data = json.loads(requests.get("http://data.taipei/youbike").content)
+				if data['retVal']['0001']['mday'] != lastDay:
+					lastDay = data['retVal']['0001']['mday']
+					with open('data/'+lastDay+'.json', 'w') as json_file:
+						json.dump(data,json_file)
+					print 'data renew!' + lastDay
+				time.sleep(30)
+		except:
+			pass
 	#data = json.loads(response.read())
 	#print data
